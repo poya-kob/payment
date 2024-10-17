777ac3ec9fc2952753f1c3116af1148051e12777
poya <poya_kob@live.com>
2024-10-17 17:35:23 +0330

    loan model temp created

--- borrow/models.py
+++ borrow/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from my_users.models import User
 
-# Create your models here.
+
+class Loan(models.Model):
+    loan_code = models.UUIDField(auto_created=True, unique=True)
+    term = models.IntegerField(null=True, blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    amount = models.DecimalField()
+    status = models.CharField(max_length=50, choices=[
+        ('pending', "در انتظار تایید"),
+        ('approved', "تایید شده"),
+        ('rejected', "رد شده"),
+        ('paid', "پرداخت شده"),
+        ('underwriting', "درحال پذیره نویسی"),
+        ('default', "پیش فرض"),
+    ])
