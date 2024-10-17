3b2af67caa541d4c019c422d72ceb8b3cbb7681f
poya <poya_kob@live.com>
2024-10-17 18:34:35 +0330

    request loan model created

--- borrow/models.py
+++ borrow/models.py
@@ -3,16 +3,34 @@ from my_users.models import User
 
 
 class Loan(models.Model):
+    """
+    تعریف وام
+    """
     loan_code = models.UUIDField(auto_created=True, unique=True)
     term = models.IntegerField(null=True, blank=True)
     start_date = models.DateField()
     end_date = models.DateField()
     amount = models.DecimalField()
     status = models.CharField(max_length=50, choices=[
+        ('end', "پایان یافته"),
+        ('underwriting', "درحال پذیره نویسی"),
+        ('default', "پیش فرض"),
+    ])
+    number_of_loan_payable = models.SmallIntegerField()
+
+
+class RequestLoan(models.Model):
+    """
+    درخواست وام
+    """
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    loan = models.ForeignKey(Loan, on_delete=models.PROTECT)
+    request_date = models.DateField()
+    refund_amount = models.DecimalField(default=None)  # باقیمانده بازپرداخت
+    status = models.CharField(max_length=50, choices=[
         ('pending', "در انتظار تایید"),
         ('approved', "تایید شده"),
         ('rejected', "رد شده"),
         ('paid', "پرداخت شده"),
-        ('underwriting', "درحال پذیره نویسی"),
         ('default', "پیش فرض"),
     ])
--- payment_collector/models.py
+++ payment_collector/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
 from my_users.models import User
+from borrow.models import Loan
 
 
-class Payment(models.Model):
+class InstallmentRePayment(models.Model):
+    """
+    بازپرداخت اقساط
+    """
     User = models.ForeignKey(User, on_delete=models.PROTECT)
+    loan = models.ForeignKey(Loan, on_delete=models.PROTECT)
     image = models.ImageField(upload_to="")
-    amount = models.IntegerField()
+    payment = models.DecimalField()
     pay_date = models.DateField()
     upload_date = models.DateField()
     is_verified = models.BooleanField(default=False)
-
