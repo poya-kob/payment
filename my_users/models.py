c24a7f3524f0259e819ac399bd9e5ba7b2231771
poya <poya_kob@live.com>
2024-10-17 16:50:19 +0330

    user_code UUidfield added to users model

--- my_users/models.py
+++ my_users/models.py
@@ -6,7 +6,6 @@ class User(AbstractUser):
     username = None
     email = None
     phone_number = models.CharField(max_length=11, unique=True)
+    user_code = models.UUIDField(auto_created=True, unique=True)
     USERNAME_FIELD = phone_number
     REQUIRED_FIELDS = []
-
-
